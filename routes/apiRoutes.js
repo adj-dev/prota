729fc23714ff3e35f80e98f4ff764f49501d587b
johniblake <johniblake93@gmail.com>
2019-06-29 16:40:52 -0500

    adjusted api to provide authstate separately from user object

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,4 +6,8 @@ router.get("/user", (req, res) => {
   req.user ? res.json(req.user) : res.json({ error: true });
 });
 
+router.get("/auth-status", (req, res) => {
+  req.user ? res.json(true) : res.json(false);
+});
+
 module.exports = router;
