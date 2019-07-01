c933a846db2d1ae693baed3d55ab3477690f0945
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-07-01 15:14:54 -0500

    Made changes to controllers and routes.

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,19 +1,24 @@
 const db = require("../models");
 
 module.exports = {
-    getAllByUser: function(req, res){ //get all projects by req.user
-        console.log(req.user);
-        //NEEDS CODE
+    getAllByUser: function(userId){ //get all projects by req.user
+        console.log(userId);
+        return db.User.findById({_id: userId}).populate('Project')
+            .then(results => results)
+            .catch(err => err);
     },
 
-    getOneById: function(req, res){ //get project by req.params.projectId
-        console.log(req.params.projectId);
-        //NEEDS CODE
+    getOneById: function(projectId){ //get project by req.params.projectId
+        console.log(projectId);
+        return db.Project.findById({_id: projectId})
+        .populate('User').populate('Sprint')
+            .then(results => results)
+            .catch(err => err);
     },
 
     create: function(req, res){ //create a project using req.body
         console.log(req.body);
-        db.Project
+        return db.Project
             .create(req.body)
             .then(results => res.json(results))
             .catch(err => res.json(err));
