6886e030e25f48e70441c9f444cd3389f62ba79f
adj-dev <andrew.johnson2073@gmail.com>
2019-07-02 14:02:33 -0500

    Cleaned up TaskListSelector component

--- client/src/components/TaskListSelector/index.js
+++ client/src/components/TaskListSelector/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import TaskList from '../TaskList';
 import "./style.css"
 
+
+
 // Declare our selector values here as variables, this way we get a helpful error if we mispell a variable vs. 
 // getting no error thrown if we mispell the string.
 const ALL = 'ALL';
@@ -9,32 +11,11 @@ const OPEN = 'OPEN';
 const IN_PROGRESS = 'IN_PROGRESS';
 const DONE = 'DONE';
 
-// Define initial state for Reducer
-
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case 'ALL':
-//       return state;
-//     case 'OPEN':
-//       return state.filter(task => task.status === OPEN);
-//     case 'IN_PROGRESS':
-//       return state.filter(task => task.status === IN_PROGRESS);
-//     case 'DONE':
-//       return state.filter(task => task.status === DONE);
-//     default:
-//       throw new Error();
-//   }
-// }
 
 
 const TaskListSelector = ({ tasks, selection }) => {
-  console.log(selection);
-
   const [allTasks, setAllTasks] = useState(tasks);
   const [selectedTasks, setSelectedTasks] = useState([]);
-  // const [chosenTasks, dispatch] = useReducer(reducer, { tasks: tasks })
-  // const [status, setStatus] = useState(OPEN);
-
 
   useEffect(() => {
     setAllTasks(tasks)
@@ -45,12 +26,6 @@ const TaskListSelector = ({ tasks, selection }) => {
     let selection = allTasks.filter(task => task.status === status);
     status === ALL ? setSelectedTasks(tasks) : setSelectedTasks(selection);
   }
-
-  // const filterTasks = useCallback(
-  //   () => {
-  //     userSelectsTasks(status)
-  //   }, [userSelectsTasks, status]
-  // )
 
   return (
     <div className="tasklist-wrapper">
