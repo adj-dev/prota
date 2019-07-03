081b5dda50bfe5129bbaf937d51a632a6339b068
johniblake <johniblake93@gmail.com>
2019-07-03 02:15:03 -0500

    improved UI of create project component

--- models/user.js
+++ models/user.js
@@ -2,26 +2,31 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: { //Github.username
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        //match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
-    },
-    avatar_url: { //Github.photos[0].value
-        type: String,
-        //required: true
-    },
-    display_name: { //Github.displayName
-        type: String,
-        //required: true
-    },
-    projects: [{
-        type: Schema.Types.ObjectId,
-        Ref: "Project"
-    }]
+  username: {
+    //Github.username
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+    //match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+  },
+  avatar_url: {
+    //Github.photos[0].value
+    type: String
+    //required: true
+  },
+  display_name: {
+    //Github.displayName
+    type: String
+    //required: true
+  },
+  projects: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Project"
+    }
+  ]
 });
 
 const User = mongoose.model("User", UserSchema);
