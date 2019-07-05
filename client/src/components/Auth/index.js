ad4d7fdd1971afc9e944788c74571bbca8456b8b
adj-dev <andrew.johnson2073@gmail.com>
2019-07-04 22:01:56 -0500

    Corrected href in login link

--- client/src/components/Auth/index.js
+++ client/src/components/Auth/index.js
@@ -6,7 +6,7 @@ export default class Auth extends Component {
     return (
       <div className="auth-container">
         {/* This href will change to /auth/github in production*/}
-        <a href="http://localhost:3001/auth/github">
+        <a href="/auth/github">
           <div className="github-button">Sign in with Github</div>
         </a>
         {/* <a href="/api/auth/github">Sign in with Github</a> */}
