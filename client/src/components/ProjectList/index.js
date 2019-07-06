17d107fd9c581dc8743ce2f8e259c1a99e113544
johniblake <johniblake93@gmail.com>
2019-07-05 22:53:00 -0500

    update profile layout

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -5,21 +5,20 @@ import "./style.css";
 export default function ProjectList({ projects, toggleCreateProjectDialog }) {
   return (
     <div className="project-list-container">
+      <div className="project-list-header">
+        Projects
+        <div
+          className="create-project-button"
+          onClick={toggleCreateProjectDialog}
+        >
+          +
+        </div>
+      </div>
       <div className="project-list">
         {projects.map((project, key) => (
-          <ProjectListItem
-            lang="en"
-            key={key}
-            id={project._id}
-            title={project.name}
-          />
+          <ProjectListItem lang="en" key={key} project={project} />
         ))}
-      </div>
-      <div
-        className="create-project-button"
-        onClick={toggleCreateProjectDialog}
-      >
-        +
+        <div className="project-list-gradient" />
       </div>
     </div>
   );
