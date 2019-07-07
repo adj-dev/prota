25a3c45d7ad0dcccb4728ca874c1fcb3fa5e9fd7
johniblake <johniblake93@gmail.com>
2019-07-07 14:18:13 -0500

    added animation and smooth scrolling to splash page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
-import Auth from "./components/Auth";
 import Project from "./pages/Project";
 import API from "./utils/API";
 import Profile from "./pages/Profile";
