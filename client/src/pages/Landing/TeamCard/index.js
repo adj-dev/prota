3408233b330b0e6ae51c21a512c2470ad9dcae0b
johniblake <johniblake93@gmail.com>
2019-07-08 00:02:55 -0500

    added basic about page

---
+++ client/src/pages/Landing/TeamCard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export default function TeamCard({ imageUrl, name, githubUrl }) {
+  return (
+    <div className="team-card">
+      <a href={githubUrl} alt={name + " Github"} target="_blank">
+        <img
+          className="profile-image"
+          src={imageUrl}
+          alt={name + " profile image"}
+        />
+      </a>
+      {name}
+    </div>
+  );
+}
