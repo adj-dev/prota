19ba10b048dc784d5a1d6452f1ba01b87d2d1c13
adj-dev <andrew.johnson2073@gmail.com>
2019-07-07 20:10:04 -0500

    Resolved issue where .populate() was called on .create(), thats a no-no

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Redirect} from 'react-router';
+import { Redirect } from 'react-router';
 import { BrowserRouter, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Auth from "./components/Auth";
@@ -51,7 +51,7 @@ class App extends Component {
                 component={Project}
               />
               <Route exact path="/welcome" component={Landing} />
-              <Redirect to="/" />
+              {/* <Redirect to="/" /> */}
             </BrowserRouter>
           </div>
         ) : null}
